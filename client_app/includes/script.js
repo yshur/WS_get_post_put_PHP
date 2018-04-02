3fcd620d11bc8a151e7feca8450c4fa024b49621
yshur <yair.shur@gmail.com>
2018-04-03 01:43:45 +0300

    finishes for 2018

--- client_app/includes/script.js
+++ client_app/includes/script.js
@@ -4,7 +4,7 @@ function sendGet() {
   var num2=$('#numB').val();
   var num3=$('#numC').val();
   var func=$('input[name=func]:checked').val();
-  $.get("http://shenkar.html5-book.co.il/2016-2017/rs/dev_70/ws_calculator/control.php?num1="+num1+"&num2="+num2+"&num3="+num3+"&func="+func+"",
+  $.get("http://shenkar.html5-book.co.il/2017-2018/dcs/dev_33/ws_calculator/controller.php?num1="+num1+"&num2="+num2+"&num3="+num3+"&func="+func+"",
     function(data) {
       $(".result1").empty();
       $(".result1").append('Result: ');
@@ -17,7 +17,7 @@ function sendPost() {
   var num5=$('#numE').val();
   var num6=$('#numF').val();
   var func1=$('input[name=func1]:checked').val();
-  $.post("http://shenkar.html5-book.co.il/2016-2017/rs/dev_70/ws_calculator/control.php",
+  $.post("http://shenkar.html5-book.co.il/2017-2018/dcs/dev_33/ws_calculator/controller.php",
     {func:func1, num1:num4, num2:num5, num3:num6},
     function(data) {
       $(".result2").empty();
@@ -31,9 +31,11 @@ function sendPut() {
   var num8=$('#numH').val();
   var num9=$('#numI').val();
   var func2=$('input[name=func2]:checked').val();
+  var data = {func:func2, num1:num7, num2:num8, num3:num9}
   $.ajax({
-    url : 'http://shenkar.html5-book.co.il/2016-2017/rs/dev_70/ws_calculator/control.php',
+    url : 'http://shenkar.html5-book.co.il/2017-2018/dcs/dev_33/ws_calculator/controller.php',
     type : 'PUT',
+	data: data,
     success : function(data){
       $(".result3").empty();
       $(".result3").append('Result: ');
